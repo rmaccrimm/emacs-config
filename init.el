06873ed5e3b5588a39acda6727ef1cecea29a7f3
Roderick MacCrimmon <r.maccrimm@gmail.com>
2020-06-01 20:27:55 -0600

    add initial config file

---
+++ init.el
@@ -0,0 +1,8 @@
+(require package)
+(setq package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(eval-when-compile (require 'use-package))
+
+(setq inhibit-startup-screen t)
+(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
