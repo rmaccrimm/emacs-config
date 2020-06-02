c3d14b0a9b1c0781fd71bea5ae82732f7e473a90
Roderick MacCrimmon <r.maccrimm@gmail.com>
2020-06-01 22:08:41 -0600

    add appearance customizations

---
+++ appearance.el
@@ -0,0 +1,29 @@
+(use-package display-line-numbers
+  :config
+  (global-display-line-numbers-mode))
+
+(use-package fill-column-indicator
+  :config
+  (setq-default fill-column 99)
+  (setq-default fci-rule-color "#3E4451")
+  (define-globalized-minor-mode global-fci-mode fci-mode
+    (lambda () (fci-mode 1)))
+  (global-fci-mode 1))
+
+(use-package snazzy-theme
+  :config
+  (load-theme 'snazzy t)
+  (set-face-attribute 'cursor nil
+                      :background "#9aedfe")
+  (set-face-attribute 'default nil
+                      :background "#282a36")
+  (set-face-attribute 'line-number nil
+                      :background "#282a36"
+                      :foreground "#78787e"))
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+(add-to-list 'default-frame-alist '(font . "Fira Code-14"))
+(set-frame-font "Fira Code-14" nil t)
+
--- init.el
+++ init.el
@@ -1,8 +1,10 @@
-(require package)
-(setq package-archives '("melpa" . "https://melpa.org/packages/"))
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
 (eval-when-compile (require 'use-package))
 
 (setq inhibit-startup-screen t)
 (setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
+
+(load-file "./appearance.el")
