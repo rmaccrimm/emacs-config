b66922464cc50ee863b0807bff353d249d058ff7
Roderick MacCrimmon <r.maccrimm@gmail.com>
2020-06-03 18:13:50 -0600

    replace snazzy-theme with custom version

--- appearance.el
+++ appearance.el
@@ -12,22 +12,17 @@
     (lambda () (fci-mode 1)))
   (global-fci-mode 1))
 
-(use-package snazzy-theme
-  :ensure t
-  :config
-  (load-theme 'snazzy t)
-  (set-face-attribute 'cursor nil
-                      :background "#9aedfe")
-  (set-face-attribute 'default nil
-                      :background "#282a36")
-  (set-face-attribute 'line-number nil
-                      :background "#282a36"
-                      :foreground "#78787e")
-  (set-face-attribute 'fringe nil
-                      :background "#282a36"))
-
-(menu-bar-mode -1)
-(tool-bar-mode -1)
+(add-to-list 'custom-theme-load-path "~/.config/emacs/themes/")
+(load-theme 'snazzy-custom t)
+(set-face-attribute 'cursor nil
+		    :background "#9aedfe")
+(set-face-attribute 'default nil
+		    :background "#282a36")
+(set-face-attribute 'line-number nil
+		    :background "#282a36"
+		    :foreground "#78787e")
+(set-face-attribute 'fringe nil
+		    :background "#282a36")
 
 (add-to-list 'default-frame-alist '(font . "Fira Code-14"))
 (set-frame-font "Fira Code-14" nil t)
