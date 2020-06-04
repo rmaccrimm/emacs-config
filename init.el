7cfb0d30e60faab704c84fe0a2ebba2a29fc10f6
Roderick MacCrimmon <r.maccrimm@gmail.com>
2020-06-03 18:20:04 -0600

    disable menu-bar and tool-bar immediately

--- init.el
+++ init.el
@@ -1,23 +1,30 @@
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+(setq inhibit-startup-screen t
+      backup-directory-alist '(("" . "~/.emacs.d/backup"))
+      delete-old-versions t)
+
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
 (eval-when-compile (require 'use-package))
 
-(setq
- inhibit-startup-screen t
- backup-directory-alist '(("" . "~/.emacs.d/backup"))
- delete-old-versions t)
-
-(setq-default electric-pair-mode t)
-
 (use-package mic-paren
   :ensure t
   :config
   (paren-activate))
+(setq-default electric-pair-mode t)
 
+(global-set-key (kbd "C-;") 'toggle-line-comment)
+(global-set-key (kbd "C-=") 'enlarge-window-horizontally)
+(global-set-key (kbd "C--") 'shrink-window-horizontally)
+
+(load-file "~/.config/emacs/term.el")
 (load-file "~/.config/emacs/appearance.el")
 (load-file "~/.config/emacs/helm.el")
 (load-file "~/.config/emacs/lsp.el")
 (load-file "~/.config/emacs/company.el")
 (load-file "~/.config/emacs/rust.el")
+
